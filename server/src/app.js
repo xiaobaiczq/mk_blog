59a56986decff0731edfa163808d23a112f5c6a0
markchen <markchen@aaxischina.com>
2018-11-27 10:41:08 -0300

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 var koa = require("koa");
 var bodyparser = require("koa-bodyparser");
 var  Router = require('koa-router')
+var path=require('path')
+const static = require('koa-static')
 
 var app = new koa();
 
@@ -10,11 +12,17 @@ app.use(bodyparser());
 //     await console.log(ctx.request.body)
 //     ctx.body = 'Hello World';
 // });
+const staticPath = './dist'
+
+app.use(static(
+    path.join( __dirname,  staticPath)
+))
 
 var router=new Router();
-router.get(/\/REST\/navList/,async (ctx,next)=>{
+router.get(/\//,async (ctx,next)=>{
+    ctx.redirect('/dist/index.html');
 })
 
-app.listen(3002, () => {
-    console.log("server start in 3002");
+app.listen(9000, () => {
+    console.log("server start in 9000");
 });
