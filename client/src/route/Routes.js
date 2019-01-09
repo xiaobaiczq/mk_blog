54cdd59c76dce4826b9080dd27547b0d13a47176
markchen <markchen@aaxischina.com>
2019-01-09 12:44:48 -0300

    update

--- client/src/route/Routes.js
+++ client/src/route/Routes.js
@@ -15,7 +15,7 @@ class Routes extends React.Component {
     }
 
     componentDidMount() {
-        this.audioAutoPlay();
+        setTimeout(this.audioAutoPlay,6000)
     }
 
     audioAutoPlay() {
@@ -53,7 +53,7 @@ class Routes extends React.Component {
                     </div>}
                 </div>
             </Router>
-            <audio controls="controls" preload id="audio" loop="loop" style={{display: "none"}}>
+            <audio controls="controls"  id="audio" loop="loop" style={{display: "none"}}>
                 <source src={playList[random]} type="audio/mpeg"/>
                 Your browser does not support the audio element.
             </audio>
