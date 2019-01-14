c3ec19eaf004d2e749d4bda822945281d9d6230d
markchen <markchen@aaxischina.com>
2019-01-14 11:28:40 -0300

    update

--- server/src/router/UserRouter.js
+++ server/src/router/UserRouter.js
@@ -30,7 +30,10 @@ userRouter.post("/REST/user/login",async (ctx,next)=>{
 
 
 userRouter.get("/REST/user/isLogin",async (ctx,next)=>{
-    var userInfo=ctx.session.userInfo;
+    var userInfo;
+    if(ctx.session!=null){
+        userInfo=ctx.session.userInfo;
+    }
     if (!userInfo ){
         ctx.body=responseUtil.output("0","未登录",null);
     } else {
